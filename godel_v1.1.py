b3df39892400db6864849caa72b6d7985832fc3f
lekersten <80839200+lekersten@users.noreply.github.com>
2024-02-19 13:27:45 +0100

    initial version using GODEL

---
+++ godel_v1.1.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+model_name = "microsoft/GODEL-v1_1-large-seq2seq"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+def generate(instruction, knowledge, dialog):
+    if knowledge != '':
+        knowledge = '[KNOWLEDGE] ' + knowledge
+    dialog = ' EOS '.join(dialog)
+    query = f"{instruction} [CONTEXT] {dialog} {knowledge}"
+    input_ids = tokenizer(f"{query}", return_tensors="pt").input_ids
+    outputs = model.generate(input_ids, max_length=128, min_length=8, top_p=0.9, do_sample=True)
+    output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return output
+
+# Instruction for a chitchat task
+instruction = f'You are a social robot that like to entertain users and be a friend to them.'
+
+# Leave the knowldge empty
+knowledge = 'I have a doctors appointment tomorrow at 4pm.'
+dialog = [
+    'Hello! How are you?',
+    'I am great! And you?.',
+    'Me as well thank you for asking.'
+]
+
+for _ in range(5):
+    dialog.append(input("\n> "))
+    response = generate(instruction, knowledge, dialog)
+
+    dialog.append(response)
+
+    print(response)
+    print()
+    print(dialog)
+
+    print()
+    print(knowledge)

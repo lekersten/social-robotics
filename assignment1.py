261ff53a6c4a457e4248a52e667dcbec0d3cd653
lekersten <80839200+lekersten@users.noreply.github.com>
2024-02-19 14:12:07 +0100

    fixed conflicts

--- assignment1.py
+++ assignment1.py
@@ -2,30 +2,6 @@ from autobahn.twisted.component import Component, run
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.util import sleep
 
-<<<<<<< Updated upstream
-@inlineCallbacks
-def main(session, details):
-    question = "Is Paul a good prof?"
-    answers = {"yes": ["yes", "sure", "yeah"], "super": ["super", "the best", "definitely"]}
-
-    answer = yield session.call("rie.dialogue.ask",
-                                question=question,
-                                answers=answers)
-
-    if answer == "yes":
-        yield session.call("rie.dialogue.say_animated",
-                           text="Good to hear that you think Paul is a good prof. I agree")
-    elif answer == "super":
-        session.call("rie.dialogue.say",
-                     text="Great. Paul is indeed a super-duper prof")
-        yield session.call("rom.optional.behavior.play", name="BlocklyHappy")
-    else:
-        yield session.call("rie.dialogue.say",
-                           text="Sorry. What did you say? Be positive about Paul")
-    yield session.call("rie.dialogue.stop")
-
-    session.leave()  # Close the connection with the robot
-=======
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 import random
@@ -60,11 +36,13 @@ def asr(frames):
 def generate(instruction, knowledge, dialog):
     if knowledge != '':
         knowledge = '[KNOWLEDGE] ' + knowledge
+
     dialog = ' EOS '.join(dialog)
     query = f"{instruction} [CONTEXT] {dialog} {knowledge}"
     input_ids = tokenizer(f"{query}", return_tensors="pt").input_ids
     outputs = model.generate(input_ids, max_length=128, min_length=8, top_p=0.9, do_sample=True)
     output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
     return output
 
 
@@ -100,26 +78,17 @@ def main(session, details):
     yield session.call("rie.dialogue.say", text="Goodbye! It was nice talking to you.")
     session.leave()
 
->>>>>>> Stashed changes
-
 wamp = Component(
     transports=[{
         "url": "ws://wamp.robotsindeklas.nl",
         "serializers": ["msgpack"],
         "max_retries": 0
     }],
-<<<<<<< Updated upstream
-    realm="rie.65ccac62c01d7b6600471940",
-=======
+
     realm="rie.65d35051ead719a540fad216",
->>>>>>> Stashed changes
 )
 
 wamp.on_join(main)
 
 if __name__ == "__main__":
-<<<<<<< Updated upstream
     run([wamp])
-=======
-    run([wamp])
->>>>>>> Stashed changes

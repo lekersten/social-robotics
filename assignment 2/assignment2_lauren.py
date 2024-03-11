68469f1faa8aab0a3c2addbc4fd498d9b00408c8
lekersten <80839200+lekersten@users.noreply.github.com>
2024-03-11 12:13:51 +0100

    changes to assignemnt2_lauren

--- assignment 2/assignment2_lauren.py
+++ assignment 2/assignment2_lauren.py
@@ -7,7 +7,8 @@ def main(session, details):
     
     yield session.call("rie.dialogue.config.language", lang="en")
     yield session.call("rom.optional.behavior.play", name="BlocklyStand")
-
+    
+    
     prep_1 = 400  # start preparation for greeting
     target_1 = 1200  # target time for greeting
     duration_1 = 3600 / 9  # duration of greeting (400ms increments)
@@ -16,35 +17,56 @@ def main(session, details):
 
     session.call("rie.dialogue.say", text="Hey you, over there!")
 
-    # yield sleep(5)
+    yield session.call("rom.actuator.motor.write", 
+                    frames = [{"time": 600, "data": {"body.arms.right.upper.pitch": -2.5}}],
+                        force=True
+                        )
+
+    yield session.call("rom.actuator.motor.write",
+                    frames=[{"time": 800, "data": {"body.arms.right.lower.roll": 1.5}},
+                            {"time": 1200, "data": {"body.arms.right.lower.roll": -1}},
+                            {"time": 1600, "data": {"body.arms.right.lower.roll": 1.5}},
+                            {"time": 2000, "data": {"body.arms.right.lower.roll": -1}},
+                            {"time": 2400, "data": {"body.arms.right.lower.roll": 1.5}},
+                            {"time": 2800, "data": {"body.arms.right.lower.roll": -1}},
+                            {"time": 3200, "data": {"body.arms.right.lower.roll": 1.5}}],
+                    force=True
+                    )
+
     session.call("rie.dialogue.say", text="Come closer, I want to ask you something")
 
-    # combine greeting and beckoning
-    yield session.call("rom.actuator.motor.write",
-                       frames=[{"time": prep_1, "data": {"body.arms.right.upper.pitch": -2.5}},
-                               {"time": duration_1*2, "data": {"body.arms.right.lower.roll": 1.5}},
-                               {"time": target_1, "data": {"body.arms.right.lower.roll": -1, "body.arms.left.upper.pitch": -0.75}},
-                               {"time": duration_1*4, "data": {"body.arms.right.lower.roll": 1.5, "body.arms.left.lower.roll": -1.7}},
-                               {"time": duration_1*5, "data": {"body.arms.right.lower.roll": -1, "body.arms.left.lower.roll": -1.25}},
-                               {"time": duration_1*6, "data": {"body.arms.right.lower.roll": 1.5, "body.arms.left.lower.roll": -1.7}},
-                               {"time": duration_1*7, "data": {"body.arms.right.lower.roll": -1, "body.arms.left.lower.roll": -1.25}},
-                               {"time": duration_1*8, "data": {"body.arms.right.upper.pitch": 0, "body.arms.right.lower.roll": 0, "body.arms.left.lower.roll": -1.7}},
-                               {"time": duration_1*9, "data": {"body.arms.left.lower.roll": -1.25}},
-                               {"time": retract, "data": {"body.arms.left.upper.pitch": 0, "body.arms.left.lower.roll": 0}}],
-                       force=True
-                       )
+    # beckoning wave
+    session.call("rom.actuator.motor.write", 
+                       frames = [{"time": 400, "data": {"body.arms.left.upper.pitch": -1}}],
+                          force=True
+                          )
+
+    session.call("rom.actuator.motor.write",
+                    frames=[{"time": 800, "data": {"body.arms.left.lower.roll": -1.7}},
+                            {"time": 1200, "data": {"body.arms.left.lower.roll": -1}},
+                            {"time": 1600, "data": {"body.arms.left.lower.roll": -1.7}},
+                            {"time": 2000, "data": {"body.arms.left.lower.roll": -1}},
+                            {"time": 2400, "data": {"body.arms.left.lower.roll": -1.7}},
+                            {"time": 2800, "data": {"body.arms.left.lower.roll": -1}}],
+                    force=True
+                    )
+    
+    yield sleep(3)
+    session.call("rom.optional.behavior.play", name="BlocklyStand")
 
 
+    yield sleep(3)
     session.call("rie.dialogue.say", text="Would you like to hear a story?")
 
-    frames = yield session.call("rie.vision.card.read")
-    print(frames[0]) 
-
-    if frames[0] == 1:
+    card = yield session.call("rie.vision.card.read")
+    card_id = card[-1]['data']['body'][0][-1]
+    print(card_id)
+ 
+    if card_id == 0:
         # Tell story
         yield sleep(5)  
 
-    elif frames[0] == 2:
+    elif card_id == 1:
         # upset robot
         yield sleep(5)
 
@@ -60,7 +82,7 @@ wamp = Component(
         "serializers": ["msgpack"],
         "max_retries": 0
     }],
-    realm="rie.65e888a2d9eb6cfb396e55ad",
+    realm="rie.65eed9c4d9eb6cfb396e7432",
 )
 
 wamp.on_join(main)

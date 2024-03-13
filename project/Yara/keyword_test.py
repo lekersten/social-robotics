54d162cb2cd71dc8e94b2fb6497362c2477baed6
YaraNimble <bikowski24@gmail.com>
2024-03-13 16:52:21 +0100

    start code

---
+++ project/Yara/keyword_test.py
@@ -0,0 +1,47 @@
+from autobahn.twisted.component import Component, run
+from twisted.internet.defer import inlineCallbacks
+from autobahn.twisted.util import sleep
+
+
+global sess
+
+
+@inlineCallbacks
+def on_keyword(frame):
+    global sess
+    c = frame["data"]["body"]["certainty"]
+    print("certainty", c, ": ", frame)
+    if "certainty" in frame["data"]["body"] and frame["data"]["body"]["certainty"] > 0.45:
+        word = frame["data"]["body"]["text"]
+        yield sess.call("rie.dialogue.say_animated", text=f"Test worked for {word}!")
+
+
+@inlineCallbacks
+def main(session, details):
+    question = "This is a test."
+    global sess
+    sess = session
+
+    yield session.call("rie.dialogue.config.language", lang="en")
+    yield session.call("rie.dialogue.keyword.language", lang="en")
+    yield session.call("rie.dialogue.say_animated", text=question)
+    yield session.call("rie.dialogue.keyword.add", keywords=["yes", "test"])
+    yield session.subscribe(on_keyword, "rie.dialogue.keyword.stream")
+    yield session.call("rie.dialogue.keyword.stream")
+
+    # yield session.leave()  # Close the connection with the robot
+
+
+wamp = Component(
+    transports=[{
+        "url": "ws://wamp.robotsindeklas.nl",
+        "serializers": ["msgpack"],
+        "max_retries": 0
+    }],
+    realm="rie.65f1b4ddd9eb6cfb396e87a8",
+)
+
+wamp.on_join(main)
+
+if __name__ == "__main__":
+    run([wamp])
